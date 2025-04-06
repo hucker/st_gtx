400b46420b876940ccf535c40ae7e16f630dbce3
Chuck Bass <chuck@acrocad.net>
2025-04-06 08:12:46 -0700

    Update st_gtx.py

--- st_gtx.py
+++ st_gtx.py
@@ -1,7 +1,25 @@
 import streamlit as st
+import ten8t
 
 st.header("GTX to CSV File Translator")
 csv = st.text_input("Enter CSV Data")
 if st.button("Convert to GTX..."):
     st.text("GTX of " + csv)
 
+uploaded_file = st.file_uploader("Choose CSV file with GTX ata")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
