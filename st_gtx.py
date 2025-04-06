a3ba6b109f64a5b921007817512217fcedb5db03
Chuck Bass <chuck@acrocad.net>
2025-04-05 19:01:09 -0700

    Update st_gtx.py

--- st_gtx.py
+++ st_gtx.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.header("GTX to CSV File Translator")
-st.write("")
 csv = st.text("Enter CS")
-if st.button("Convert to GTX...")
+if st.button("Convert to GTX..."):
     st.text("GTX of " + csv)
 
